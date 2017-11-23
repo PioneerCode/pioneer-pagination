5d017c8b423f840b7f9c01fdd87f34ba036e2026
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-11-23 10:18:17 -0600

    Add next page tests

--- test/Pioneer.Pagination.Tests/NextPageTests.cs
+++ test/Pioneer.Pagination.Tests/NextPageTests.cs
@@ -1,10 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Linq;
+using Xunit;
 
 namespace Pioneer.Pagination.Tests
 {
-    class NextPageTests
+    /// <summary>
+    /// Next Page Tests
+    /// </summary>
+    public class NextPageTests
     {
+        private readonly PaginatedMetaService _sut = new PaginatedMetaService();
+
+        [Fact]
+        public void NextPageNotDisplayed()
+        {
+            var result = _sut.GetMetaData(10, 10, 1);
+            Assert.False(result.NextPage.Display, "Expected: Next Page Display = false");
+        }
+
+        [Fact]
+        public void NextPageDisplayed()
+        {
+            var result = _sut.GetMetaData(10, 9, 1);
+            Assert.True(result.NextPage.Display, "Expected: Next Page Displayed = true");
+        }
+
+        [Fact]
+        public void NextPageNextPageOneIndexPastLastNumericShown()
+        {
+            var result = _sut.GetMetaData(100, 6, 10);
+            Assert.True(result.NextPage.PageNumber == result.Pages.First(x => x.IsCurrent).PageNumber + 1,
+                string.Format("Expected: Next Page number == {0}", result.Pages.First(x => x.IsCurrent).PageNumber + 1));
+        }
     }
 }
