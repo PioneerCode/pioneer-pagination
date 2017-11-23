e4b30eef9af07d2f65fd2db1aa56ef52b0dab3ef
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-11-23 10:18:51 -0600

    Add previous page tests

--- test/Pioneer.Pagination.Tests/PreviousPageTests.cs
+++ test/Pioneer.Pagination.Tests/PreviousPageTests.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Xunit;
 
 namespace Pioneer.Pagination.Tests
 {
-    class PreviousPageTests
+    /// <summary>
+    /// Previous Page Tests
+    /// </summary>
+    public class PreviousPageTests
     {
+        private readonly PaginatedMetaService _sut = new PaginatedMetaService();
+
+        [Fact]
+        public void PreviousPageNotDisplayed()
+        {
+            var result = _sut.GetMetaData(10, 1, 1);
+            Assert.False(result.PreviousPage.Display, "Expected : Previous Page Displayed = false");
+        }
+
+        [Fact]
+        public void PreviousPageDisplayed()
+        {
+            var result = _sut.GetMetaData(10, 2, 1);
+            Assert.True(result.PreviousPage.Display, "Expected : Previous Page Displayed = true");
+        }
     }
 }
