bb6e23913cbc812c0a9cf62957f9b9011f8d8fa1
ryn0 <ryanmichaelwilliams@gmail.com>
2018-04-22 17:23:22 -0700

    unit tests for previous/ next page text

---
+++ test/Pioneer.Pagination.Tests/ControlTextTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pioneer.Pagination.Tests
+{
+    public class ControlTextTests
+    {
+        private readonly PioneerPaginationTagHelper _sut = new PioneerPaginationTagHelper();
+        private readonly PaginatedMetaService _metaData = new PaginatedMetaService();
+
+        TagHelperContext context = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                Guid.NewGuid().ToString("N"));
+
+        TagHelperOutput output = new TagHelperOutput("ul",
+            new TagHelperAttributeList(),
+            (result, encoder) =>
+            {
+                var tagHelperContent = new DefaultTagHelperContent();
+                tagHelperContent.SetHtmlContent(string.Empty);
+                return Task.FromResult<TagHelperContent>(tagHelperContent);
+            });
+
+        [Fact]
+        public void CanShowCustomPreviousPageText()
+        {
+            var previousPageText = "Backward";
+            _sut.Info = _metaData.GetMetaData(10, 10, 1);
+            _sut.PreviousPageText = previousPageText;
+
+            _sut.Process(context, output);
+            var markupResult = output.Content.GetContent();
+
+            Assert.Contains(previousPageText, markupResult);
+        }
+
+        [Fact]
+        public void CanShowCustomNextPageText()
+        {
+            var nextPageText = "Forward";
+            _sut.Info = _metaData.GetMetaData(10, 1, 1);
+            _sut.NextPageText = nextPageText;
+
+            _sut.Process(context, output);
+            var markupResult = output.Content.GetContent();
+
+            Assert.Contains(nextPageText, markupResult);
+        }
+    }
+}
