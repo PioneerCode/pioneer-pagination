a275a81fb8d089967c622d920c3909358b893f04
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-05-24 18:01:47 -0500

    Strengthen expected messaging on failed tests.

--- test/Pioneer.Pagination.Tests/ClampTests.cs
+++ test/Pioneer.Pagination.Tests/ClampTests.cs
@@ -13,42 +13,42 @@ namespace Pioneer.Pagination.Tests
         public void NextPageIsLastPageInCollectionWhenRequestedPageIsGreatedThenCollection()
         {
             var result = _sut.GetMetaData(10, 50, 1);
-            Assert.True(result.NextPage.PageNumber == 10, "Expected: Last Page ");
+            Assert.True(result.NextPage.PageNumber == 10, "Expected: Next page should be last page in collection.");
         }
 
         [Fact]
         public void PreviousPageIsLastPageMinusOneInCollectionWhenRequestedPageIsGreatedThenCollection()
         {
             var result = _sut.GetMetaData(10, 100, 1);
-            Assert.True(result.PreviousPage.PageNumber == 9, "Expected: Last Page ");
+            Assert.True(result.PreviousPage.PageNumber == 9, "Expected: Previous page should be last page in collection minus 1.");
         }
 
         [Fact]
         public void PreviousPageIsFirstPageInCollectionWhenRequestedPageIsZero()
         {
             var result = _sut.GetMetaData(10, 0, 1);
-            Assert.True(result.PreviousPage.PageNumber == 1, "Expected: First Page ");
+            Assert.True(result.PreviousPage.PageNumber == 1, "Expected: Previous page should be first page in collection.");
         }
        
         [Fact]
         public void NextPageIsSecondPageInCollectionWhenRequestedPageIsZero()
         {
             var result = _sut.GetMetaData(10, 0, 1);
-            Assert.True(result.NextPage.PageNumber == 2, "Expected: First Page ");
+            Assert.True(result.NextPage.PageNumber == 2, "Expected: Next page should be second page in collection.");
         }
 
         [Fact]
         public void PreviousPageIsFirstPageInCollectionWhenRequestedPageIsNegative()
         {
             var result = _sut.GetMetaData(10, -1, 1);
-            Assert.True(result.PreviousPage.PageNumber == 1, "Expected: First Page ");
+            Assert.True(result.PreviousPage.PageNumber == 1, "Expected: Previous page should be first page in collection.");
         }
 
         [Fact]
         public void NextPageIsSecondPageInCollectionWhenRequestedPageIsNegative()
         {
             var result = _sut.GetMetaData(10, -1, 1);
-            Assert.True(result.NextPage.PageNumber == 2, "Expected: First Page ");
+            Assert.True(result.NextPage.PageNumber == 2, "Expected: Next page should be second page in collection.");
         }
     }
 }
