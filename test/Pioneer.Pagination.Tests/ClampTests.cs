1faf9ceb20ee83e79b86efbee39cd10881be2416
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-05-24 16:38:47 -0500

    Add Clamp Tests

---
+++ test/Pioneer.Pagination.Tests/ClampTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Pioneer.Pagination.Tests
+{
+    /// <summary>
+    /// Clamp Tests
+    /// </summary>
+    public class ClampTests
+    {
+        private readonly PaginatedMetaService _sut = new PaginatedMetaService();
+
+        [Fact]
+        public void LastPageDisplayed()
+        {
+            var result = _sut.GetMetaData(10, 11, 1);
+            Assert.True(result.NextPage.PageNumber == 10, "Expected: Last Page ");
+        }
+
+        [Fact]
+        public void FirstPageDisplayed()
+        {
+            var result = _sut.GetMetaData(10, 0, 1);
+            Assert.True(result.NextPage.PageNumber == 1, "Expected: First Page ");
+        }
+    }
+}
